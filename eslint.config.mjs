b90681836dc3b5ff5b4def0456e63ccf8d889a2b
Maxim Efremov <m.efremov@volta.space>
2025-03-02 17:08:02 +0300

    chore: optimize build config and type declarations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,45 @@ import importPlugin from 'eslint-plugin-import';
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
   {
-    ignores: ['node_modules', 'dist', '.parcel', '.parcel-cache'],
+    ignores: ['node_modules', 'dist'],
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2022,
+      },
+    },
+    plugins: {
+      eslint: eslintPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      ...eslintPlugin.configs.recommended.rules,
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external', 'internal']],
+          'newlines-between': 'always',
+        },
+      ],
+      'import/no-unresolved': 'error',
+      'import/no-duplicates': 'error',
+      'max-len': [
+        'error',
+        {
+          code: 100,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreComments: true,
+        },
+      ],
+      'linebreak-style': ['error', 'unix'],
+    },
   },
   {
     files: ['**/*.ts'],
